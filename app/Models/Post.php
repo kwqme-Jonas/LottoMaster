125639bec97afe9207d52ce13ba2ef528e755c8d
Jonas Donkor <donkoronas00@gmail.com>
2024-06-16 13:40:22 +0000

    made changes to blog page

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,12 +5,21 @@ namespace App\Models;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Post extends Model
 {
     use HasFactory;
 
 
+    protected $casts = [
+        'published_at' => 'datetime',
+
+    ];
+
+    public function author(){
+        return $this->belongsTo(User::class, 'user_id');  
+    }
 
 
 
@@ -23,6 +32,17 @@ class Post extends Model
     public function scopeFeatured($query)
     {
         $query->where('featured', true);
+    }
+
+    public function getExcerpt(){
+        return str::limit(strip_tags($this->body), 150);
+    }
+
+    public function getReadingTime()
+    { 
+        $mins = round(str_word_count($this->body) / 250); 
+
+         return ($mins < 1) ? 1 : $mins;
     }
 }
 
